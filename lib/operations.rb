13ef9c66f88a085e041b97d95867a3c1667a0055
Paulina Salem <paulinasalem@yahoo.com>
2018-04-15 01:24:11 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,14 +1,14 @@
 def unsafe?(speed)
 if speed>60
 	true
+elsif speed<40
+	true
+elsif 40<speed<60
+	false
 end
 end
 
 
 def not_safe?(speed)
-if speed<40
-	true
-elsif 40<speed<60
-	false
-end
+
 end
